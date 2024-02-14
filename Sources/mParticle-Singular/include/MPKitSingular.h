d57b13ab7e94463300aae52a9bbf98dd93aef220
tapasya <tapasya@singular.net>
2024-02-14 16:05:31 +0530

    added extended attribution info

--- Sources/mParticle-Singular/include/MPKitSingular.h
+++ Sources/mParticle-Singular/include/MPKitSingular.h
@@ -13,6 +13,7 @@
 #define SINGULAR_DEEPLINK_KEY @"singular_deeplink"
 #define SINGULAR_PASSTHROUGH_KEY @"singular_passthrough"
 #define SINGULAR_IS_DEFERRED_KEY @"singular_is_deferred"
+#define SINGULAR_QUERY_PARAMS @"singular_query_params"
 
 @interface MPKitSingular : NSObject <MPKitProtocol>
 
@@ -23,5 +24,9 @@
 @property (nonatomic, strong, nullable) NSArray<NSDictionary<NSString *, id> *> *userIdentities;
 
 +(void)setSKANOptions:(BOOL)skAdNetworkEnabled isManualSkanConversionManagementMode:(BOOL)manualMode withWaitForTrackingAuthorizationWithTimeoutInterval:(NSNumber* _Nullable)waitTrackingAuthorizationWithTimeoutInterval withConversionValueUpdatedHandler:(void(^_Nullable)(NSInteger))conversionValueUpdatedHandler;
+
++ (void)setDeviceAttributionCallback:(void(^_Nullable)(NSDictionary*_Nullable))deviceAttributionHandler;
+
++ (void)setCustomSDID:(NSString *_Nullable)customSdid sdidReceivedHandler:(void(^_Nullable)(NSString *_Nullable))sdidReceivedHandler didSetSdidHandler:(void(^_Nullable)(NSString *_Nullable))setSdidHandler;
 
 @end
